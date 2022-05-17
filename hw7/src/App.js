d6879e60055338da642ce54bed191166a9faa6db
b <mfps30521@yahoo.com.tw>
2022-05-17 17:03:47 +0800

    hw7

---
+++ hw7/src/App.js
@@ -0,0 +1,12 @@
+import Login from './cgu_login'
+import './App.css';
+
+function App() {
+  return (
+    <div id="login">
+      {Login()}
+      </div>
+  );
+}
+
+export default App;
---
+++ hw7/src/cgu_login.js
@@ -0,0 +1,17 @@
+const Login=()=>{
+    var output=[];
+    output.push(
+    <><div>
+        <h2>CGU Login</h2>
+    </div><div>
+            <p>User Name</p><input></input>
+            <p>Password</p><input type="password"></input>
+        </div>
+        <div>
+            <button>登入</button>
+        </div>
+        </>
+        )
+        return output;
+}
+export default Login;  
