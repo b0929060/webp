2b39cc94e3a621f39473786bb6da2f68ba821893
b <mfps30521@yahoo.com.tw>
2022-05-18 17:17:41 +0800

    hw8

---
+++ hw8/src/App.js
@@ -0,0 +1,15 @@
+import Login from './cgu_login';
+import React, {Component} from 'react';
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div>
+      {Login()}
+    </div>
+
+  );
+}
+
+export default App;
---
+++ hw8/src/cgu_login.js
@@ -0,0 +1,34 @@
+import Button from '@mui/material/Button';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import LockIcon from '@mui/material/Icon';
+const Login=()=>{
+    var output=[];
+    output.push(
+    <><div>
+         <IconButton color="primary" aria-label="add an alarm">
+  <LockIcon /></IconButton>
+        <h2>CGU Login</h2>
+    </div><div>
+            <p>Email</p><input></input>
+            <p>Password</p><input type="password"></input>
+        </div>
+        <div>
+            <Button variant="contained" color="primary">sign in</Button>
+        </div>
+        <CardActions>
+      <Button size="small" color="primary">
+        Forgat password?
+      </Button>
+      <Button size="small" color="primary">
+        Don't have an account?
+      </Button>
+      <Button size="small" color="primary">
+        Sign Up
+      </Button>
+    </CardActions>
+    </>
+        )
+        return output;
+}
+export default Login;  
