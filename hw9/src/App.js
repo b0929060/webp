684dbb7835ce0cc358ca5358f17cc074778dd079
b <mfps30521@yahoo.com.tw>
2022-06-18 20:36:34 +0800

    hw9修正

--- hw9/src/App.js
+++ hw9/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Todo from './Todo';
 
 function App() {
   return (
     <div id="root">
+      <Todo />
     </div>
   );
 }
--- hw9/src/Todo.js
+++ hw9/src/Todo.js
@@ -1,37 +1,62 @@
 import React from "react";
-import ReactDOM from "react-dom";
-class Todolist extends React.Component { 
+class Todo extends React.Component { 
     constructor(props) { 
-    super(props); 
-    this.state = { 
-    value: '', 
-    itemList: [], 
-    }; 
-    this.handleChange = this.handleChange.bind(this); 
-    this.handleSubmit = this.handleSubmit.bind(this); 
+      super(props); 
+      this.state = { 
+        value: '', 
+        itemList: [],
+        itemDone: [], 
+      }; 
+      this.handleChange = this.handleChange.bind(this); 
+      this.handleSubmit = this.handleSubmit.bind(this); 
     } 
     handleChange(event) { 
-    this.setState({value: event.target.value}); 
+      this.setState({value: event.target.value}); 
     } 
     handleSubmit(event) { 
-    this.state.itemList.push(this.state.value); 
-    this.setState({value: '', itemList: this.state.itemList,}); 
-    event.preventDefault(); 
-    } 
+      this.state.itemList.push(this.state.value);
+      this.state.itemDone.push(false); 
+      this.setState({value: '', itemList: this.state.itemList,itemDone:this.state.itemDone}); 
+      event.preventDefault(); 
+    }
+    handleDelete(index){
+        const temp=this.state.itemList.filter((item,i)=>{
+            return index!==i
+        })
+        const temp2=this.state.itemDone.filter((item,i)=>{
+            return index!==i
+        })
+        this.setState({value:'',itemList:temp,itemDone:temp2});
+    }
+    handleDone(index){
+        const temp=this.state.itemDone;
+        if(temp[index]===true){
+            temp[index]=false;
+        }else{
+            temp[index]=true;
+        }
+        this.setState({itemDone:temp});
+    }
     render() { 
-    return ( 
-    <div> 
-    <form onSubmit={this.handleSubmit}> 
-    <label> <h1>CGU Todo list</h1>
-    <input type="text" placeholder="add a new todo..."value={this.state.value} onChange={this.handleChange} /> 
-    </label> 
-    <input type="submit" value="Add" />
-    </form> 
-    <ul style={{ textAlign: 'left' }}> 
-    {this.state.itemList.map((item, index) => <li key={`item_${index}`}>{item}</li>) } 
-    </ul> 
-    </div> 
-    ); 
+      return ( 
+        <div className="todolist_wrapper"> 
+        <form onSubmit={this.handleSubmit} className="todolist_addform"> 
+          <h1 className="todolist_title">CGU Todo list</h1><br></br>
+            <input type="text" value={this.state.value} onChange={this.handleChange} className="todolist_input"placeholder="add a new todo..."/> 
+          <input type="submit" value="Add" className="todolist_addbtn"/> 
+        </form> 
+        <div className="todolist_list-wrapper"> 
+            {this.state.itemList.map((item, index) => 
+            <div key={`item_${index}`}>
+                <input type="checkbox" id={index} onClick={()=>this.handleDone(index)} className="todolist_checkbox"></input>
+                {this.state.itemDone[index] ? <label for={index} style={{textDecoration:'line-through'}} className="todolist_itemlist">{item}</label>: <label for={index} className="todolist_itemlist">{item}</label>}
+                <button onClick={()=>this.handleDelete(index)} className="todolist_rmbtn">X</button>
+            </div>
+          ) } 
+        </div> 
+        </div> 
+      ); 
     } 
-   } 
-   ReactDOM.render(<Todolist />,document.getElementById('root'));
+  } 
+
+export default Todo;
